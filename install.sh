0e82849fdc55373ada1e0c4ce360020f91e0fa40
shakinnn <post@itprofit32.ru>
2018-04-01 09:55:17 +0300

    1

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "inastalling asteriks please wait!"
+cp ./asteriks /usr/sbin/asteriks
+chmod +x /usr/sbin/asteriks
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+
+
+
+
+
+
